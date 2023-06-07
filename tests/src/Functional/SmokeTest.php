702e892bed8ecd33e1bc7de57a2f29daaf78831c
Antonio De Marco <antonio@nuvole.org>
2023-06-07 15:28:55 +0200

    OEL-2326: Initial commit.

---
+++ tests/src/Functional/SmokeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_graphql\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Smoke test for project scaffolding.
+ */
+class SmokeTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'node',
+    'system',
+    'oe_graphql',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->drupalLogin($this->drupalCreateUser(['administer graphql configuration'], '', TRUE));
+  }
+
+  /**
+   * Tests that the test site is reachable.
+   */
+  public function testSiteIsReachable(): void {
+    $this->drupalGet('/admin/config/graphql');
+    $this->assertSession()->pageTextContains('Servers');
+  }
+
+}
